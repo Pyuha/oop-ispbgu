7d8dc3ad9d3555e41fa8f4299fa06db97f1900be
Pyuha <125832554+Pyuha@users.noreply.github.com>
2023-10-18 01:18:16 +0300

    Update task_1.py
    Исправлен метод решения задачи без удаления/добавления элемента

--- ЛР1/task_1.py
+++ ЛР1/task_1.py
@@ -1,6 +1,10 @@
 numbers = [2, -93, -2, 8, None, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
-numbers.remove(None)
-average = sum(numbers)/(len(numbers)+1)
 
-numbers.insert(4, average)
-print("Измененный список:", numbers)
+len_of_numbers = len(numbers)
+sum_of_numbers = sum(numbers[:4]+numbers[5:])
+
+middle_arithmetic = sum_of_numbers/len_of_numbers
+
+numbers[4] = middle_arithmetic
+
+print(numbers)
