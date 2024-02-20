2f7599ce15d5804979e29134a27f552744566eab
Pyuha <125832554+Pyuha@users.noreply.github.com>
2024-02-20 14:58:44 +0300

    Update main.py

--- ЛР3/main.py
+++ ЛР3/main.py
@@ -9,31 +9,62 @@ class Book:
     def __repr__(self):
         return f"{self.__class__.__name__}(name={self._name!r}, author={self._author!r})"
 
+    @property
+    def name(self) -> str:
+        return self._name
+
+    @property
+    def author(self) -> str:
+        return self._author
+
 
 class PaperBook(Book):
     def __init__(self, name: str, author: str, pages: int):
         super().__init__(name, author)
         self.pages = pages
-        if isinstance(self.pages, int) == False:
-            raise TypeError("Неверный формат данных о кол-ве страниц.")
 
-    def __str__(self):
-        return f"Бумажная книга {self._name}. Автор {self._author}. Страниц {self.pages}"
+    def __str__(self) -> str:
+        return f"{super().__str__()}. Число страниц {self._pages}"
 
     def __repr__(self):
-        return f"{self.__class__.__name__}(name={self._name!r}, author={self._author!r}, pages={self.pages!r})"
+        return f"{self.__class__.__name__}(name={self._name!r}, author={self._author!r}, pages={self._pages!r})"
+
+    @property
+    def pages(self) -> int:
+        return self._pages
+
+    @pages.setter
+    def pages(self, pages: int) -> None:
+        if isinstance(self.pages, int):
+            if pages > 0:
+                self._pages = pages
+            else:
+                raise TypeError("Неверный формат данных, он не может быть отрицательным или равен нулю.")
+        else:
+            raise TypeError('Неверный формат данных, он должен быть целочисленного типа.')
 
 
 class AudioBook(Book):
     def __init__(self, name: str, author: str, duration: float):
         super().__init__(name, author)
         self.duration = duration
-        if isinstance(self.duration, float) == False:
-            raise TypeError("Неверный формат данных о продолжительности книги.")
-
 
     def __str__(self):
-        return f"Аудио-книга {self._name}. Автор {self._author}. Продолжительность {self.duration}"
+        return f"{super().__str__()}. Продолжительность {self._duration}"
 
     def __repr__(self):
-        return f"{self.__class__.__name__}(name={self._name!r}, author={self._author!r}, duration={self.duration!r})"
+        return f"{self.__class__.__name__}(name={self._name!r}, author={self._author!r}, duration={self._duration!r})"
+
+    @property
+    def duration(self) -> float:
+        return self._duration
+
+    @duration.setter
+    def duration(self, duration: float) -> None:
+        if isinstance(self.duration, float):
+            if duration > 0:
+                self._duration = duration
+            else:
+                raise TypeError("Неверный формат данных, он не может быть отрицательным или равен нулю.")
+        else:
+            raise TypeError('Неверный формат данных, он должен быть вещественного типа')
